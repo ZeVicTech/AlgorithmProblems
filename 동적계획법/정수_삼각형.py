d6bb132959f64d8d4c87c0f060f018f8f4359690
ZeVicTech <kys980501@gmail.com>
2023-01-26 15:19:21 +0900

    자동정렬 되어 수정됨

--- 동적계획법/정수_삼각형.py
+++ 동적계획법/정수_삼각형.py
@@ -1,17 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43105
 
 def solution(triangle):
-    
+
     list = [item[:] for item in triangle]
-    
+
     for i in range(len(triangle) - 1):
         for j in range(len(triangle[i])):
             temp = triangle[i][j] + list[i+1][j]
-            triangle[i+1][j] = max(temp,triangle[i+1][j])
-            
+            triangle[i+1][j] = max(temp, triangle[i+1][j])
+
             temp = triangle[i][j] + list[i+1][j+1]
-            triangle[i+1][j+1] = max(temp,triangle[i+1][j+1])
-            
+            triangle[i+1][j+1] = max(temp, triangle[i+1][j+1])
+
     answer = max(triangle[len(triangle)-1])
-    
+
     return answer
--- 동적계획법/퇴사2.py
+++ 동적계획법/퇴사2.py
@@ -1,4 +1,4 @@
-#https://www.acmicpc.net/problem/15486
+# https://www.acmicpc.net/problem/15486
 
 # 다른 사람의 아이디어 보고 푼 문제
 # 정말 해설을 봐도 이해가 잘 되지 않는 문제였다.
@@ -9,19 +9,17 @@ n = int(input())
 time = [0]
 pay = [0]
 for i in range(n):
-    t,p = map(int,input().split())
+    t, p = map(int, input().split())
     time.append(t)
     pay.append(p)
 
 dp = [0]*(n+2)
 
-#dp[i]는 i일 전까지 일했던 금액
-for i in range(1,n+2):
+# dp[i]는 i일 전까지 일했던 금액
+for i in range(1, n+2):
     if dp[i] < dp[i-1]:
         dp[i] = dp[i-1]
     if i < n+1 and time[i] + i <= n+1:
-        dp[i+time[i]] = max(pay[i]+dp[i],dp[i+time[i]])
+        dp[i+time[i]] = max(pay[i]+dp[i], dp[i+time[i]])
 
 print(dp[-1])
-
-
