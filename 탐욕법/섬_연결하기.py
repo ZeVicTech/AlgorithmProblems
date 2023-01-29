0dc1d4f687be1b30e72a16e2fb982ea54c80cac3
ZeVicTech <kys980501@gmail.com>
2023-01-29 14:39:39 +0900

    섬 연결하기 문제 추가

--- 탐욕법/섬_연결하기.py
+++ 탐욕법/섬_연결하기.py
@@ -2,7 +2,34 @@
 
 # 크루스칼 알고리즘을 활용해서 해결해야함
 
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
 def solution(n, costs):
+    parent = [i for i in range(n)]
+
+    costs.sort(key = lambda x: x[2])
+
+    sum = 0
+
+    for a,b,cost in costs:
+        a = find_parent(parent,a)
+        b = find_parent(parent,b)
+        if a != b:
+            union_parent(parent,a,b)
+            sum += cost
+
+    return sum
 
 
-solution(4,[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
+solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]])
