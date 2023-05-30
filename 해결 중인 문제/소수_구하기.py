faa8522865930f09d7d1d74d7c99bb2e3bb9a50e
ZeVicTech <kys980501@gmail.com>
2023-05-30 23:21:03 +0900

    소수 구하기 문제 수정

--- 해결 중인 문제/소수_구하기.py
+++ 해결 중인 문제/소수_구하기.py
@@ -1,13 +1,16 @@
 # https://www.acmicpc.net/problem/1929
+
+# 개선해야할 점: 에라토스테니스의 체를 이용해볼 것!
+
 import math
 
 m,n = map(int,input().split())
 
 def is_prime(x):
+    if x == 1:
+        return False
     for i in range(2,int(math.sqrt(x))+1):
-        if x == 2:
-            return True
-        elif x%i == 0:
+        if x%i == 0:
             return False
     return True
 
